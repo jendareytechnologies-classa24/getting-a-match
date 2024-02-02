5cbc96d1fa38e7cbaee362448c18c0a11ee15fe5
emywizy <emeka.laplace@gmail.com>
2024-02-02 20:00:04 +0000

    lets go there

---
+++ src/main/java/com/chasegameshow/getting/a/match/GettingAMatchApplication.java
@@ -0,0 +1,13 @@
+package com.chasegameshow.getting.a.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingAMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingAMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chasegameshow/getting/a/match/GettingAMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chasegameshow.getting.a.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingAMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
